86ad570918a8329594f9eb4dc1c4c9b3f528f1e5
thalapathy26 <101509047+thalapathy26@users.noreply.github.com>
2022-03-13 23:45:00 +0700

    rajnisha

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "raj&nisha" && b=="26-03-2017")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
